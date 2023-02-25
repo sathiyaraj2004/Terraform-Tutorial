3e983f8f3aaedc79116a711e5b16e9a0895970e6
sathiyaraj2004 <112636292+sathiyaraj2004@users.noreply.github.com>
2023-02-25 20:04:14 +0530

    Update provider.tf

--- aws-instance-first-script/provider.tf
+++ aws-instance-first-script/provider.tf
@@ -1,4 +1,9 @@
-provider "aws" {
-  region  = "${var.region}"
-  
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.0"
+      region  = "${var.region}"
+    }
+  }
 }
