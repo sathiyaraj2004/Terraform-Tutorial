10726a15721935315951f5fac8687c0df0410a56
sathiyaraj2004 <112636292+sathiyaraj2004@users.noreply.github.com>
2023-02-25 20:13:48 +0530

    Update variables.tf

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -1,11 +1,7 @@
-variable "region" {
-  default = "us-east-1"
+variable "ami_id" {
+  type = map(string)
 }
 
-variable "ami_id" {
-  type = "map"
-
-  default = {
-    us-east-1    = "ami-035b3c7efe6d061d5"
-  }
+variable "region" {
+  type = string
 }
